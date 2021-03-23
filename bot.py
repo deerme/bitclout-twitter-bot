04b58739b4f6c5c81a981df10a8ccb91aba22208
gemt33 <78438718+gemt33@users.noreply.github.com>
2021-03-23 10:43:49 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 # Python 3.8.5
 import cloudscraper
+import tweepy
 
+url = 'https://api.bitclout.com'
 scraper = coudscraper.create_scraper()
-print(scrapper.post(url, data = {"PostHashHex":"4f61aaafd26a8f1e6676e29885237a665e7514f4f24dfbf3e7e3d04c321296f5","ReaderPublicKeyBase58Check":"BC1YLi9NmWoASziGZYzhfoKXB6fxyCCRbxTDH5Sep2KCm7rPHTCwx5g","FetchParents":True,"CommentOffset":0,"CommentLimit":20,"AddGlobalFeedBool":False}, headers={'Origin' : 'https://bitclout.com', 'Accept' : 'application/json, text/plain, */*', 'Accept-Encoding' : 'gzip, deflate, br', 'Accept-Language' : 'en-US', 'Connection' : 'keep-alive', 'Content-Length' : '251', 'Content-Type' : 'application/json', 'DNT' : '1', 'Host' : 'api.bitclout.com', 'Sec-GPC' : '1', 'TE' : 'Trailers'}).text)
+print(scraper.post(url, data = {"PostHashHex":"4f61aaafd26a8f1e6676e29885237a665e7514f4f24dfbf3e7e3d04c321296f5","ReaderPublicKeyBase58Check":"BC1YLi9NmWoASziGZYzhfoKXB6fxyCCRbxTDH5Sep2KCm7rPHTCwx5g","FetchParents":True,"CommentOffset":0,"CommentLimit":20,"AddGlobalFeedBool":False}, headers={'Origin' : 'https://bitclout.com', 'Accept' : 'application/json, text/plain, */*', 'Accept-Encoding' : 'gzip, deflate, br', 'Accept-Language' : 'en-US', 'Connection' : 'keep-alive', 'Content-Length' : '251', 'Content-Type' : 'application/json', 'DNT' : '1', 'Host' : 'api.bitclout.com', 'Sec-GPC' : '1', 'TE' : 'Trailers'}).text)
